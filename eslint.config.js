41d5897493e5c2b4d765d473d8c4fea9181aa5e7
Midorisy <837239572@qq.com>
2026-04-02 16:12:42 +0800

    Keep calm and commit

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  type: 'app',
+  // TypeScript and Vue are autodetected, you can also explicitly enable them:
+  typescript: true,
+  vue: true,
+  // Disable jsonc and yaml support
+  jsonc: false,
+  yaml: false,
+})
