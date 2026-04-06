4207371d6263f53d28431132e1d4774e5cc9af12
Midorisy <837239572@qq.com>
2026-04-06 15:36:21 +0800

    feat: 初步封装axios，包含实例拦截器

---
+++ src/service/createService.ts
@@ -0,0 +1,106 @@
+import type { AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
+import axios from 'axios'
+import URL from './baseConfig'
+
+// 定义自己的axios配置项，继承自axios的配置项
+// 生成实例时可以传入自己专属的拦截器
+interface IInterceptorceptors {
+  requestSuccessFn?: (config: InternalAxiosRequestConfig) => InternalAxiosRequestConfig
+  requestFailFn?: (error: any) => Promise<any>
+  responseSuccessFn?: (response: AxiosResponse) => AxiosResponse
+  responseFailFn?: (error: any) => Promise<never>
+}
+
+// 实例传入配置项的类型
+interface IConfig extends AxiosRequestConfig {
+  baseURL?: string
+  timeout?: number
+  interceptors?: IInterceptorceptors
+
+}
+
+// 主要的axios请求类
+class Service {
+  service: AxiosInstance
+  constructor(config?: IConfig) {
+    this.service = axios.create({
+      baseURL: URL.BASE_URL,
+      timeout: URL.TIMEOUT,
+
+      ...config,
+    })
+
+    // 全局请求拦截器
+    this.service.interceptors.request.use(
+      // 请求成功时调用
+      (config) => {
+        return config
+      },
+      // 请求失败时调用
+      (error: any) => {
+        return Promise.reject(error)
+      },
+    )
+
+    // 全局响应拦截器
+    this.service.interceptors.response.use(
+      // 响应成功时调用
+      // 这里的类型还需要进一步修改
+      (response) => {
+        console.log('已在全局拦截')
+        return response
+      },
+      // 响应失败时调用
+      (error: any) => {
+        // 可能会出现某些提示，需要根据实际情况处理
+        return Promise.reject(error)
+      },
+    )
+
+    // 处理实例拦截器
+    if (config?.interceptors) {
+      this.service.interceptors.request.use(
+        config.interceptors?.requestSuccessFn,
+        config.interceptors?.requestFailFn,
+      )
+      this.service.interceptors.response.use(
+        config.interceptors?.responseSuccessFn,
+        config.interceptors?.responseFailFn,
+      )
+    }
+  }
+
+  request(config: IConfig) {
+    return this.service.request(config)
+  }
+
+  get(config: IConfig) {
+    return this.request({
+      ...config,
+      method: 'GET',
+    })
+  }
+
+  post(config: IConfig) {
+    return this.request({
+      ...config,
+      method: 'POST',
+    })
+  }
+
+  put(config: IConfig) {
+    return this.request({
+      ...config,
+      method: 'PUT',
+    })
+  }
+
+  delete(config: IConfig) {
+    return this.request({
+      ...config,
+      method: 'DELETE',
+    })
+  }
+}
+
+export default Service
---
+++ src/service/index.ts
@@ -0,0 +1,25 @@
+import type { InternalAxiosRequestConfig } from 'axios'
+import Service from './createService'
+
+const service1 = new Service({
+  interceptors: {
+    requestSuccessFn: (config) => {
+      console.log('实例1的拦截器')
+      return config
+    },
+  },
+})
+
+const service2 = new Service({
+  interceptors: {
+    requestSuccessFn: (config) => {
+      console.log('实例2的拦截器')
+      return config
+    },
+  },
+})
+
+export default {
+  service2,
+  service1,
+}
