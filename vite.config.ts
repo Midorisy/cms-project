dc05c7dfdc8d4e5251370447e30e93480cd203d7
Midorisy <837239572@qq.com>
2026-04-06 18:34:48 +0800

    feat: 引入ElementPlus配置，按需引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins:
-  [vue()],
+  [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
