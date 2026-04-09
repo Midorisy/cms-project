dbb3f303fe8255dd8d83e595d59d4610ba8063b8
Midorisy <837239572@qq.com>
2026-04-09 16:06:48 +0800

    feat: 完成v1路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,7 @@
+import type { RUserData } from '@/types/Login/LoginTypes'
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { USER_DATA_KEY } from '@/global/constant'
+import { local } from '@/utils/storage'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -23,6 +26,27 @@ const router = createRouter({
       component: () => import('@/views/NotFound/NotFound.vue'),
     },
   ],
+})
+
+router.beforeEach((to, _from) => {
+  const userData: RUserData = local.getItem(USER_DATA_KEY)
+  // 如果不是登录页，且没有登陆，就跳转登录页
+  if (to.path !== '/login') {
+    // 如果不是前往登录页
+    if (userData?.token) {
+      // 如果有token,就放行
+      return true
+    }
+    else {
+      // 如果没有token,就跳转登录页
+      router.replace({
+        path: '/login',
+      })
+    }
+  }
+  else {
+    console.log('来到登录页')
+  }
 })
 
 export default router
