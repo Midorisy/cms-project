576108f719ff355784e21b5111d1524b19c8c279
Midorisy <837239572@qq.com>
2026-04-10 16:49:58 +0800

    feat: home页的简单搭建

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,15 +1,31 @@
 <script setup lang="ts">
-import useHomeStore from '@/store/Home/useHomeStore'
-
-const homeStore = useHomeStore()
 </script>
 
 <template>
   <div class="home">
-    {{ homeStore.count }}
+    <el-container>
+      <el-aside width="250px">
+        Aside
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.home{
+  height: 100%;
+  .el-container{
+    height: 100%;
+    .el-aside{
+      background-color: pink;
+    }
+    .el-container{
+      background-color: aquamarine;
+    }
+  }
+}
 </style>
