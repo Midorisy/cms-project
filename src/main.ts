573730c118d2658676db516a73ec98c894b004a4
Midorisy <837239572@qq.com>
2026-04-18 03:21:34 +0800

    fix: 解决刷新页面时，动态路由未添加并且白屏的问题

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createApp } from 'vue'
+import { hasLocalDynamicRoutes } from '@/router/home/pushDynamicRoutes'
 import App from './App.vue'
 import router from './router'
 import pinia from './store'
@@ -15,6 +16,8 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 
 app.use(pinia)
+// 如果本地已经保存了路由权限数据，就直接添加对应的动态路由
+hasLocalDynamicRoutes()
 app.use(router)
 
 app.mount('#app')
